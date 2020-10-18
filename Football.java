1f264108b6896f8d4d76c3dab715f763b9b2c799
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-18 22:21:32 +0530

    invoking write method three times

---
+++ Football.java
@@ -0,0 +1,14 @@
+class Goat
+{
+	public static void main(String[] params)
+	{
+		System.out.println("Who is the best player in the world");
+		write();
+		write();
+		write();
+	}
+	public static void write()
+	{
+		System.out.println("Lionel Messi");
+	}
+}
