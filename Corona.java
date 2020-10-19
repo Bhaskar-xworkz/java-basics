6ca85959f4d4258b4bfa2f17ca7901ee0a4626c8
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-19 21:59:05 +0530

    datatype examples(set2)

---
+++ Corona.java
@@ -0,0 +1,20 @@
+public class Corona
+{
+	public static void main(String[] params)
+	{
+		String virus = "COVID19 ";
+		long totalCases = 40435880;
+		int activeCases = 9116717;
+		long deaths = 30198946;
+		byte distance = 6;
+		String symptoms = "fever,dry cough,tiredness";
+		char blood = 'O';
+		
+		System.out.println(virus + " alert!!!");
+		System.out.println("total covid-19 cases "+ totalCases);
+		System.out.println("total activeCases "+ activeCases );
+		System.out.println("total deaths " + deaths);
+		System.out.println("most common symptoms " +symptoms);
+		System.out.println("please maintain " + distance + " feets social distance");
+		System.out.println("according to study blood group type "+ blood +" may offer some protection against covid-19 infection");
+}}
