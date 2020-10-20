069a37135b508fa6c0e3e58353387dabe2001f37
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-20 12:48:26 +0530

    final  keyword in java

---
+++ Developer.java
@@ -0,0 +1,20 @@
+public class Developer
+{
+	public static void main(String[] args)
+	{
+		System.out.println("details of developer");
+	
+	final long aadharNumber = 536369058502l;
+	
+		System.out.println("aadhar Number  : " + aadharNumber);
+	
+	final long phoneNumber = 9972018356l;
+	
+		System.out.println("phone Number : " + phoneNumber);
+	
+	final String emailId = "bhaskarxorkz@gmail.com";
+	
+		System.out.println("Email Id : " + emailId);
+	}
+}
+		
