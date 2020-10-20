b62a80133aab34eb3bafbdc689f5a29a070c2abf
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-20 12:49:15 +0530

    invoking/calling methods

---
+++ Trainee.java
@@ -0,0 +1,17 @@
+public class Trainee
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Details of Trainee");
+		
+		email("bhaskarxworkz@gmail.com");
+		programmingLanguage("java");
+	}
+	public static void email(String mailId)
+	{
+		System.out.println("Email : "+ mailId);
+	}
+	public static void programmingLanguage(String language)
+	{
+		System.out.println("programming Language : "+ language);
+}}
