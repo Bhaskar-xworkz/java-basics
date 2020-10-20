396f157d2979098a611982bc8e36070fb5f39684
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-20 11:53:00 +0530

    datatypes Examples(set3)

---
+++ DataTypes.java
@@ -0,0 +1,38 @@
+public class DataTypes
+{
+public static void main(String[] args)
+{
+	byte alphabets;
+	alphabets =  26;
+	System.out.println("The total alphabets in english is " + alphabets);
+	
+	short salaryCreditedInFirstMonth;
+	salaryCreditedInFirstMonth = 30000;
+	System.out.println("my First Month Salary is " + salaryCreditedInFirstMonth);
+	
+	int capacityOfEmiratesStadium;
+	capacityOfEmiratesStadium = 60260;
+	System.out.println("The Total Capacity of Emirates Stadium is " + capacityOfEmiratesStadium);
+	
+	long distanceFromEarthToSun;
+	distanceFromEarthToSun = 149600000;
+	System.out.println("Distance betweeen the Earth and the Sun is " + distanceFromEarthToSun +"kms");
+	
+	char save;
+	save = 's';
+	System.out.println("use control + "+ save + " to save the file");
+	
+	String name;
+	name = "Dr B R Ambedkar";
+	System.out.println(name + " is the father of Indian Constitute");
+	
+	boolean messiIsGoat = true;
+	System.out.println(messiIsGoat);
+	
+	float gravitationalConstant = 6.674f;
+	double standardUncertainity = 0.00015;
+	
+}
+}
+	
+	
