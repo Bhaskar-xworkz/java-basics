c6e4dee281ab41ed060735b7ff35446469f3abe5
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-21 19:40:28 +0530

    collection of boolean values in an array

---
+++ Quiz.java
@@ -0,0 +1,17 @@
+public class Quiz
+{
+	public static void main(String[] args)
+	{
+		System.out.println("fans vote");
+		fans();
+	}
+	public static void fans()
+	{
+		boolean messiIsGoat = true;
+		boolean ronaldoIsGoat = false;
+		boolean[] footballGreatests = {messiIsGoat,ronaldoIsGoat};
+		System.out.println("Messi is Greatest of All Time : " + messiIsGoat);
+		System.out.println("Ronaldo is Greatest of All Time : " + ronaldoIsGoat);
+	}
+	
+}
