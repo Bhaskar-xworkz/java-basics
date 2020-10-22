de3c9bc980b3268d4bfad5c85eec5c773675e378
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-22 17:42:51 +0530

    passing arguments in method

---
+++ Nations.java
@@ -0,0 +1,16 @@
+public class Nations
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Top Nations in the World :)");
+		nations("USA");
+		nations("China");
+		nations("India");
+		nations("Russia");
+		
+	}
+	public static void nations(String nameOftheCountry)
+	{
+		System.out.println("countryName :" + nameOftheCountry);
+	}
+}
