40ca5b573bcab44b3e76dff405afc20aa2fd4cd4
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-22 17:45:16 +0530

    index and length of an array

---
+++ Training.java
@@ -0,0 +1,15 @@
+public class Training{
+
+public static void main(String[] trainees)
+{
+	
+	System.out.println("JVM invoked main");
+	System.out.println("trainees "+ trainees);
+	
+	int sizeOfMainParam=trainees.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	System.out.println("exit main");
+}
+
+
+}
