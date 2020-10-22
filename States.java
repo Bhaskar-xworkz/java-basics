a435f10d3dded1be3b18e285df2f0dc29ce9369f
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-22 17:48:19 +0530

    passing arguments in main method

---
+++ States.java
@@ -0,0 +1,11 @@
+public class States
+{
+	public static void main(String[] states)
+	{
+		
+		System.out.println("List of States" + states);
+		int numberOfStates = states.length;
+		System.out.println(numberOfStates);
+		System.out.println("Number One State in India : " + states[0]);
+	}
+}
