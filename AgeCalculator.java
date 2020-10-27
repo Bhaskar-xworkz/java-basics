fbaf9edd31a8c4f5025dbf6fb3c91fc875662c33
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-10-27 22:38:31 +0530

    return keyword in java

---
+++ AgeCalculator.java
@@ -0,0 +1,9 @@
+public class AgeCalculator{
+    public static void main(String[] args)
+	{
+		byte age = 18;
+		System.out.println("Entered Age :"+ age);
+		String childAge = Age.ageDefinition(age);
+		System.out.println(childAge);
+	}
+}
