f824e1a773120f37126fe6fe44e0461a65c3b07a
Bhaskar-xworkz <72941111+Bhaskar-xworkz@users.noreply.github.com>
2020-11-07 00:04:12 +0530

    for-loops

---
+++ Nations.java
@@ -0,0 +1,12 @@
+public class Nations{
+	public static void main(String[] countries){
+		
+		for(byte msg=0;msg<=countries.length;msg++)
+		{
+		System.out.println("country at index  "+msg);
+		String countryName=countries[msg];
+		System.out.println(countryName);
+		
+		}
+	} 
+}
